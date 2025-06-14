e3cf84b77498fc7345e0ded7a0a7abc0c71b67b4
Daneyling <est.dgutierrezc0201@uml.edu.ni>
2025-06-13 19:13:40 -0600

    Agregado ejercicio2 en JavaScript

---
+++ ejercicios-js/ejercicio2.js
@@ -0,0 +1,6 @@
+const numero = Math.floor(Math.random() * 100);
+if (numero % 2 === 0) {
+    console.log(`El numero ${numero} es par`);
+} else {
+    console.log(`El  numero ${numero} es impar`);
+}
