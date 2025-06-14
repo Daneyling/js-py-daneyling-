44cfba405973701509e203cb65d7087d217cc13f
Daneyling <est.dgutierrezc0201@uml.edu.ni>
2025-06-14 12:32:00 -0600

    agregado ejercicio3 en JavaScript

--- ejercicios-js/ejercicio3.js
+++ ejercicios-js/ejercicio3.js
@@ -1,4 +1,14 @@
-function sumar (a, b) {
-    return a + b;
-}
-console.log(sumar(5, 8));
+const palabras = [
+  "Aceituna", "Murciélago", "Educación", "Aeropuerto",
+  "Otorrinolaringólogo", "Euforia", "Aceite",
+  "Paleontólogo", "Arquitectura", "Hipopótamo"
+];
+
+const vocales = ['A', 'E', 'I', 'O', 'U', 'Á', 'É', 'Í', 'Ó', 'Ú'];
+
+const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+const encontradas = palabra.toUpperCase().split('').filter(letra => vocales.includes(letra));
+
+console.log(`Palabra: ${palabra}`);
+console.log(`Vocales encontradas: ${encontradas.join(', ')}`);
+console.log(`Cantidad de vocales: ${encontradas.length}`);
