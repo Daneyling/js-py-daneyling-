e4d88b042295adfcf27d57ff120372a77c200d9c
Daneyling <est.dgutierrezc0201@uml.edu.ni>
2025-06-14 12:41:12 -0600

    agregado ejercicio6 en JavaScript

--- ejercicios-js/ejercicio6.js
+++ ejercicios-js/ejercicio6.js
@@ -1,8 +1,11 @@
-const numero = -3;
-if (numero > 0) {
-    console.log("Es positivo");
-} else if (numero < 0) {
-    console.log("Es negativo");
-} else {
-    console.log("Es cero");
+const n = Math.floor(Math.random() * 10) + 1;
+
+function factorial(num) {
+  let resultado = 1;
+  for (let i = 1; i <= num; i++) {
+    resultado *= i;
+  }
+  return resultado;
 }
+
+console.log(`${n}! = ${factorial(n)}`);
