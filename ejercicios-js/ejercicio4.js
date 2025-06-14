e8bd55e7798176671c2b3dd83735ed2b51bb6faa
Daneyling <est.dgutierrezc0201@uml.edu.ni>
2025-06-14 12:35:28 -0600

    agregado ejercicio4 en JavaScript

--- ejercicios-js/ejercicio4.js
+++ ejercicios-js/ejercicio4.js
@@ -1,3 +1,6 @@
+const numero = Math.floor(Math.random() * 10) + 1;
+
+console.log(`Tabla del ${numero}`);
 for (let i = 1; i <= 10; i++) {
-    console.log(i)
+  console.log(`${numero} x ${i} = ${numero * i}`);
 }
