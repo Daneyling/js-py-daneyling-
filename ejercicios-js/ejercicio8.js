42f14cc42fe62f05d76ee33df8d984ff19f14474
Daneyling <est.dgutierrezc0201@uml.edu.ni>
2025-06-15 19:50:28 -0600

    agregado ejercicio8 en Javascript

--- ejercicios-js/ejercicio8.js
+++ ejercicios-js/ejercicio8.js
@@ -1,8 +1,5 @@
-function factorial(n) {
-    let resultado = 1 ; 
-    for (let i = 1; i <= n; i++) {
-        resultado *=i;
-    }
-    return resultado;
-}
-console.log(factorial(5));
+const numeros = [5, 8, 12, 3, 7, 10];
+const suma = numeros.reduce((a, b) => a + b, 0);
+
+console.log(`Array: ${numeros}`);
+console.log(`Sumatoria: ${suma}`);
