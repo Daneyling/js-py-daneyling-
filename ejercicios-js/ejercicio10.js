72017685a4e641644c5d6998297f96341f85b69f
Daneyling <est.dgutierrezc0201@uml.edu.ni>
2025-06-15 19:57:18 -0600

    agregado ejercicio10 en Javascript

--- ejercicios-js/ejercicio10.js
+++ ejercicios-js/ejercicio10.js
@@ -1,6 +1,10 @@
-function esPalindromo(palabra) {
-    const invertida = palabra.split('').reverse().join('');
-    return palabra === invertida;
+const longitud = Math.floor(Math.random() * (40 - 16 + 1)) + 16;
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+let contraseña = '';
+
+for (let i = 0; i < longitud; i++) {
+  contraseña += chars[Math.floor(Math.random() * chars.length)];
 }
 
-console.log(esPalindromo("reconocer"));  // true
+console.log(`Longitud: ${longitud}`);
+console.log(`Contraseña generada: ${contraseña}`);
