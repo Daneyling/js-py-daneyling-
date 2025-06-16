17bfdb044b35c766b2108f0daf68a2b8c310d218
Daneyling <est.dgutierrezc0201@uml.edu.ni>
2025-06-15 19:54:29 -0600

    agregado ejercicio9 en Javascript

--- ejercicios-js/ejercicio9.js
+++ ejercicios-js/ejercicio9.js
@@ -1,7 +1,7 @@
-let contador = 0;
-for (let i = 1; i <= 100; i++) {
-    if (i % 2 === 0) {
-        contador++;
-    }
-}
-console.log('Hay ${contador} números pares entre 1 y 100');
+const numeros = [23, 5, 78, 12, 45, 3, 90];
+const mayor = Math.max(...numeros);
+const menor = Math.min(...numeros);
+
+console.log(`Array: ${numeros}`);
+console.log(`Mayor: ${mayor}`);
+console.log(`Menor: ${menor}`);
